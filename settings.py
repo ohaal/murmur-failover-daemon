d1b705251747aa19efa8db3db23349f55971999d
Oddbjørn <oddbjorn.haaland@gmail.com>
2013-06-26 19:18:40 -0400

    OCD settings cleanup...

--- settings.py
+++ settings.py
@@ -2,12 +2,12 @@
 data = {
     'main': {
         'murmur': {
-            'path': "/home/murmur/murmur", # Absolute path
+            'path': "/home/murmur/murmur",  # Absolute path
             'dbfile': "murmur.sqlite",      # Relative path
             'cfgfile': "murmur.ini",        # Relative path
             'pidfile': "murmur.pid",        # Relative path
             'executable': "murmur.x86",     # Relative path
-            'host': "",    # Do NOT use the hostname configured with multi IP's!
+            'host': "",    # Do NOT use the hostname configured with multi IPs!
             'port': 64738, # Default: 64738
         },
         'ssh': {
@@ -25,8 +25,8 @@ data = {
             'logpath': "/home/murmur/murmur-failover/murmur-failover.log",
             'errpath': "/home/murmur/murmur-failover/stderr.log"
         },
-        'interval': { # VALUES ARE IN SECONDS
-            'ping': 3, # How often to check if main Murmur is alive
+        'interval': {    # VALUES ARE IN SECONDS
+            'ping': 3,   # How often to check if main Murmur is alive
             'sync': 600, # How often to sync DB and CFG files
         }
     }
